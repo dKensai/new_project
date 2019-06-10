e1934554968edd728c84efc1238cfb6a3d57d0a9
Just Me <some@mail.com>
2019-06-10 22:12:49 +0300

    Gitignore test

---
+++ bootstrap/app.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Slim\App();
+
+$container = $app->getContainer();
+
+$container['view'] = function($container){
+	$view = new \Slim\Views\Twig(__DIR__ . '/../views',[
+		'cache' => false
+	])
+	$basePath = rtrim(str_replace('index.php','',$container['request']->getUri()->getBasePatch()),'');
+	$view->addExtension(new Slim\Views\TwigExtension($container['router'],$basePath));
+
+	return $view;
+}
+
+$app->get('/',function($request,$response){
+	return $this->view->render($response,'view.twig')
+})
+
+$app->get('/contact',function($request,$response){
+	return $this->view->render($response,'contact.twig')
+})
+
+$app->get('login',function($request,$response){
+	return $this->view->render($response,'login.twig')
+})
+
+
+$app->get('/other',function($request,$response){
+	// other
+})
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../bootsrap/.php';
+
+
+$app->run();
